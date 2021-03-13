2555ae8a533b21026d55f0c24a2f3265a260c997
moon_ <m3252@naver.com>
2021-03-13 16:57:27 +0900

    feat : 자바 8 API 의 기본 메소드와 스태틱 메소드

--- src/main/java/interfaceChange/App.java
+++ src/main/java/interfaceChange/App.java
@@ -1,12 +1,57 @@
 package interfaceChange;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+
 public class App {
 
     public static void main(String[] args){
-        Foo foo = new DefaultFoo("moon");
+        /*Foo foo = new DefaultFoo("moon");
         foo.printName();
         foo.printNameUpperCase();
 
-        Foo.printAnything();
+        Foo.printAnything();*/
+
+        List<String> names = new ArrayList<>();
+        names.add("moon");
+        names.add("kim");
+        names.add("lee");
+        names.add("foo");
+
+//        names.forEach(System.out::println); // 메소드 레퍼런스
+
+//        Spliterator<String> spliterator = names.spliterator();
+//        Spliterator<String> stringSpliterator = spliterator.trySplit();
+//        while (spliterator.tryAdvance(System.out::println));
+//        System.out.println("==============");
+//        while (stringSpliterator.tryAdvance(System.out::println));
+
+
+//        for(String name : names){
+//            System.out.println(name);
+//        }
+
+//        long m = names.stream().map(String::toUpperCase)
+//                .filter(s -> s.startsWith("M"))
+//                .count();
+//
+//        System.out.println(m);
+
+//        names.removeIf(s -> s.startsWith("m"));
+//        names.forEach(System.out::println);
+
+//        names.sort(String::compareToIgnoreCase);
+//        names.forEach(System.out::println);
+
+        Comparator<String> comparator = String::compareToIgnoreCase;
+        names.sort(comparator.reversed());
+        names.forEach(System.out::println);
+
+
+
+
+
     }
 }
